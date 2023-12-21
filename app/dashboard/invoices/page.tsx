c9ab615df22f406ebe546d510137e183b193da25
Irfanul Haque Sajid <irfanhsajid@gmail.com>
2023-12-21 23:35:00 +0600

    acme nextjs learning starter

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InvoicesPage = () => {
+    return (
+        <div>
+            <h1>This is Invoices Page</h1>
+        </div>
+    );
+};
+
+export default InvoicesPage;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashBoardPage = () => {
+    return (
+        <div>
+            <h1>This is the dashboard Page</h1>
+        </div>
+    );
+};
+
+export default DashBoardPage;
